6086176c83931fd7f799f945f4a6b84b217b236c
Shin <maulana.husni@gmedia.co.id>
2017-09-22 09:22:26 +0700

    Update berkala

---
+++ maulanacustommodul/src/main/java/com/maulana/custommodul/ApiVolley.java
@@ -0,0 +1,155 @@
+package com.maulana.custommodul;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Shin on 2/24/2017.
+ */
+
+public class ApiVolley {
+
+    public ApiVolley(final Context context, JSONObject jsonBody, String requestMethod, String REST_URL, final String successDialog, final String failDialog, final int showDialogFlag, final String user, final String pass , final VolleyCallback callback){
+
+        /*
+        NOTE: you have to customize this class before you use it (haeder, etc)
+        context : Application context
+        jsonBody : jsonBody (usually be used for POST and PUT)
+        requestMethod : GET, POST, PUT, DELETE
+        REST_URL : Rest API URL
+        successDialog : custom Dialog when success call API
+        failDialog : custom Dialog when failed call API
+        showDialogFlag : 1 = show successDialog / failDialog with filter
+        callback : return of the response
+        */
+
+        final String requestBody = jsonBody.toString();
+
+        int method = 0;
+
+        switch(requestMethod.toUpperCase()){
+
+            case "GET" :
+                method = Request.Method.GET;
+                break;
+            case "POST" :
+                method = Request.Method.POST;
+                break;
+            case "PUT" :
+                method = Request.Method.PUT;
+                break;
+            case "DELETE" :
+                method = Request.Method.DELETE;
+                break;
+            default: method = Request.Method.GET;
+                break;
+        }
+
+        //region initial of stringRequest
+        StringRequest stringRequest = new StringRequest(method, REST_URL, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+
+                // Important Note : need to use try catch when parsing JSONObject, no need when parsing string
+
+                if(response == null || response.equals("null")) {
+
+                    Toast.makeText(context, context.getResources().getString(R.string.api_unauthorized), Toast.LENGTH_LONG).show();
+                    callback.onError(context.getResources().getString(R.string.api_unauthorized));
+                }
+
+                try {
+
+                    callback.onSuccess(response);
+                    ShowCustomDialog(context, showDialogFlag, successDialog);
+
+                } catch (Exception e) {
+
+                    e.printStackTrace();
+//                    Toast.makeText(context, e.toString(), Toast.LENGTH_LONG).show();
+                    Toast.makeText(context, context.getResources().getString(R.string.api_error), Toast.LENGTH_LONG).show();
+                    callback.onError(e.toString());
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+
+            public void onErrorResponse(VolleyError error) {
+                callback.onError(error.toString());
+                ShowCustomDialog(context,showDialogFlag,failDialog);
+                callback.onError(error.toString());
+                return;
+            }
+        }) {
+
+            // Request Header
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Content-Type", "application/json");
+                params.put("Client-Service", "frontend-client");
+                params.put("Auth-Key", "gmedia_psp");
+                params.put("user", user);
+                params.put("pass", pass);
+                return params;
+            }
+
+            @Override
+            public String getBodyContentType() {
+                return String.format("application/json; charset=utf-8");
+            }
+
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                try {
+                    return requestBody == null ? null : requestBody.getBytes("utf-8");
+                } catch (UnsupportedEncodingException uee) {
+                    VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s",
+                            requestBody, "utf-8");
+                    return null;
+                }
+            }
+        };
+        //endregion
+
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        // retry when timeout
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy(
+                30*60*1000, /*DefaultRetryPolicy.DEFAULT_MAX_RETRIES*/0,DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+        ));
+        stringRequest.setShouldCache(false);
+        requestQueue.add(stringRequest);
+        requestQueue.getCache().clear();
+
+    }
+
+    // interface for call callback from response API
+    public interface VolleyCallback{
+        void onSuccess(String result);
+        void onError(String result);
+    }
+
+    public void ShowCustomDialog(Context context, int flag, String message){
+        if(flag == 1){
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+        }
+    }
+}
---
+++ maulanacustommodul/src/main/java/com/maulana/custommodul/SessionManager.java
@@ -0,0 +1,165 @@
+package com.maulana.custommodul;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
+import java.util.HashMap;
+
+public class SessionManager {
+	// Shared Preferences
+	SharedPreferences pref;
+	
+	// Editor for Shared preferences
+	Editor editor;
+	
+	// Context
+	Context context;
+	
+	// Shared pref mode
+	int PRIVATE_MODE = 0;
+	
+	// Sharedpref file name
+	private static final String PREF_NAME = "GmediaUser" + R.string.app_name;
+	
+	// All Shared Preferences Keys
+	private static final String IS_LOGIN = "IsLoggedIn";
+	public static final String TAG_UID = "uid";
+	public static final String TAG_NIK = "nik";
+	public static final String TAG_NAMA = "nama";
+	public static final String TAG_USERNAME = "username";
+	public static final String TAG_PASSWORD = "password";
+	public static final String TAG_TOKEN = "token";
+	public static final String TAG_EXP = "expired_at";
+	public static final String TAG_LEVEL = "level";
+	public static final String TAG_SAVED = "saved";
+	public static final String TAG_AREA = "area";
+	public static final String TAG_FLAG = "flag";
+
+	// Constructor
+	public SessionManager(Context context){
+		this.context = context;
+		pref = this.context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+		editor = pref.edit();
+	}
+	
+	/**
+	 * Create login session
+	 * */
+	public void createLoginSession(String uid, String nik, String nama, String username, String password, String saved, String token, String exp, String level, String area, String flag){
+
+		editor.putBoolean(IS_LOGIN, true);
+		
+		editor.putString(TAG_UID, uid);
+		
+		editor.putString(TAG_NIK, nik);
+
+		editor.putString(TAG_NAMA, nama);
+
+		editor.putString(TAG_USERNAME, username);
+
+		editor.putString(TAG_PASSWORD, password);
+
+		editor.putString(TAG_SAVED, saved); // value is 0 or 1
+
+		editor.putString(TAG_TOKEN, token);
+
+		editor.putString(TAG_EXP, exp);
+
+		editor.putString(TAG_LEVEL, level);
+
+		editor.putString(TAG_AREA, area);
+
+		editor.putString(TAG_FLAG, flag);
+		// commit changes
+		editor.commit();
+	}
+	
+	/**
+	 * Get stored session data
+	 * */
+	public HashMap<String, String> getUserDetails(){
+		HashMap<String, String> user = new HashMap<String, String>();
+		// user uid
+		user.put(TAG_UID, pref.getString(TAG_UID, null));
+		
+		// user nik
+		user.put(TAG_NIK, pref.getString(TAG_NIK, null));
+
+		user.put(TAG_NAMA, pref.getString(TAG_NAMA, null));
+
+		user.put(TAG_USERNAME, pref.getString(TAG_USERNAME, null));
+
+		user.put(TAG_PASSWORD, pref.getString(TAG_PASSWORD, null));
+
+		user.put(TAG_SAVED, pref.getString(TAG_SAVED, null));
+
+		user.put(TAG_TOKEN, pref.getString(TAG_TOKEN, null));
+
+		user.put(TAG_EXP, pref.getString(TAG_EXP, null));
+
+		user.put(TAG_LEVEL, pref.getString(TAG_LEVEL, null));
+
+		user.put(TAG_AREA, pref.getString(TAG_AREA, null));
+
+		user.put(TAG_FLAG, pref.getString(TAG_FLAG, null));
+		// return user
+		return user;
+	}
+
+	public String getUserInfo(String key){
+		return pref.getString(key, null);
+	}
+
+	public String getUser(){
+		return pref.getString(TAG_NAMA, null);
+	}
+
+	public String getLevel(){
+		return pref.getString(TAG_LEVEL, null);
+	}
+
+	/**
+	 * Clear session details
+	 * */
+	public void logoutUser(Intent logoutIntent){
+
+		// Clearing all data from Shared Preferences
+		try {
+			editor.clear();
+			editor.commit();
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+
+		logoutIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		context.startActivity(logoutIntent);
+		((Activity)context).finish();
+		((Activity)context).overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+	}
+	
+	/**
+	 * Quick check for login
+	 * **/
+	// Get Login State
+	public boolean isLoggedIn(){
+		if(getUserDetails().get(TAG_UID) != null){
+			return true;
+		}else{
+			return false;
+		}
+		/*return pref.getBoolean(IS_LOGIN, false);*/
+	}
+
+	public boolean isSaved(){
+		if(getUserDetails().get(TAG_SAVED) != null && getUserDetails().get(TAG_SAVED).equals("1")){
+
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+}
