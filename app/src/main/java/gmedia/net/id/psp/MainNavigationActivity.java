a236d10e284e9352891878f5c39f246943237a31
Shinmaul <maulana.husni@gmedia.co.id>
2018-08-23 09:23:52 +0700

    - History Penjualan DS : diakses dari Riwayat Penjualan - Market Survey : Survey Outlet - Pengajuan Deposit: langsung checklist mana yang dipilih setujui dan mana yang di tolak - Outlet Location : hanya bisa diakses oleh SPV dan setelah menyimpan lokasi baru, harap diverifikasi lewat TAB "Butuh Verifikasi" pada halaman yang sama

--- app/src/main/java/gmedia/net/id/psp/MainNavigationActivity.java
+++ app/src/main/java/gmedia/net/id/psp/MainNavigationActivity.java
@@ -230,6 +230,7 @@ public class MainNavigationActivity extends RuntimePermissionsActivity
                                         .setPositiveButton("Update Sekarang", new DialogInterface.OnClickListener() {
                                             @Override
                                             public void onClick(DialogInterface dialog, int which) {
+                                                dialogVersion.dismiss();
                                                 Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
                                                 startActivity(browserIntent);
                                             }
@@ -245,6 +246,7 @@ public class MainNavigationActivity extends RuntimePermissionsActivity
                                         .setPositiveButton("Update Sekarang", new DialogInterface.OnClickListener() {
                                             @Override
                                             public void onClick(DialogInterface dialog, int which) {
+                                                dialogVersion.dismiss();
                                                 Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
                                                 startActivity(browserIntent);
                                             }
